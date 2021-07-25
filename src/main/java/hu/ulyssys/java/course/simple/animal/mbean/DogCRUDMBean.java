3b594dcc87b23d53e08e8825a7a00fbc863a3bd4
Kiss Adrián Imre <kisswadrian857@gmail.com>
2021-07-25 21:24:31 +0200

    Added xhtml files to project

--- src/main/java/hu/ulyssys/java/course/simple/animal/mbean/DogCRUDMBean.java
+++ src/main/java/hu/ulyssys/java/course/simple/animal/mbean/DogCRUDMBean.java
@@ -1,4 +1,35 @@
 package hu.ulyssys.java.course.simple.animal.mbean;
 
-public class DogCRUDMBean {
+
+import hu.ulyssys.java.course.simple.animal.entity.Dog;
+import hu.ulyssys.java.course.simple.animal.service.DogService;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class DogCRUDMBean implements Serializable {
+
+    private List<Dog> list;
+
+    @Inject
+    private DogService dogService;
+
+    @PostConstruct
+    private void init(){
+        list = dogService.getALl();
+    }
+
+    public List<Dog> getList() {
+        return list;
+    }
+
+    public void setList(List<Dog> list) {
+        this.list = list;
+    }
 }
